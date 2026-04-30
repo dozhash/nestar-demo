79156ffd65f9928d7d71e45f0019d0d23e54b6fe
Shakhzod <shakhzod.ds@gmail.com>
2026-04-30 16:33:03 +0500

    feat: integration Header and Footer via Layouts

---
+++ pages/property/detail.tsx
@@ -0,0 +1,8 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Box, Container, Stack } from "@mui/material";
+
+const PropertDetail = () => {
+  return <Container>PROPERTY DETAIL</Container>;
+};
+
+export default withLayoutBasic(PropertDetail);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,9 @@
-const PropertyList = () => {
-  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
-  return <div>Property List</div>;
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Box, Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return <Container>PROPERTY LIST</Container>;
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
