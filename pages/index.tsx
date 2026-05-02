4f5d58dec88b45b8341de4bd78a48b49dcb0b3a5
Shakhzod <shakhzod.ds@gmail.com>
2026-05-02 16:24:03 +0500

    fix: homePage raw development

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
+
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TopProperties from "@/libs/components/homepage/TopProperties";
 
 const Home: NextPage = () => {
   return (
     <Stack className={"home-page"}>
-      <Stack>
-        <Stack className="container">Trend Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Popular Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Advertisement</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Agents</Stack>
-      </Stack>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
     </Stack>
   );
 };
