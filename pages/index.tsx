74001e745a3d36eba387596b87402bb1765020a4
Shakhzod <shakhzod.ds@gmail.com>
2026-05-07 06:36:14 +0500

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,34 @@ import Advertisement from "@/libs/components/homepage/Advertisement";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 10,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {
+          // "memberId": "69dd000950120993d06ad75b",
+          // "locationList": ["SEOUL", "BUSAN"],
+          // "pricesRange": {"start": 10000, "end": 100000}
+        },
+      },
+    },
+  });
+  console.log("getPropertiesData => ", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
