4ebea2798c5d1a3cb0bb7bf87661de1b34a4c20b
Shakhzod <shakhzod.ds@gmail.com>
2026-05-07 06:02:26 +0500

    feat: develop mobile version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,29 @@
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
-
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import Advertisement from "@/libs/components/homepage/Advertisement";
-
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const Home: NextPage = () => {
-  return (
-    <Stack className={"home-page"}>
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>HOMEPAGE MOBILE</Stack>;
+  } else {
+    return (
+      <Stack className={"home-page"}>
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 
 export default withLayoutMain(Home);
--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,8 +1,15 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Box, Container, Stack } from "@mui/material";
 
 const PropertDetail = () => {
-  return <Container>PROPERTY DETAIL</Container>;
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>PROPERTY DETAIL MOBILE</Stack>;
+  } else {
+    return <Container>PROPERTY DETAIL</Container>;
+  }
 };
 
 export default withLayoutBasic(PropertDetail);
